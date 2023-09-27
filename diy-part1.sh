8ca0dd23f3f29eef68e38a7bdb58549c8496ca84
Leeroy Lee <Sanluli36li@outlook.com>
2023-09-27 17:46:37 +0800

    clone luci-mod-adGuardhome

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome --depth=1
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon --depth=1
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config --depth=1
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash --depth=1
